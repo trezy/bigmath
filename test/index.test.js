2204103b2a72c130cddcf83061d312bf9272178e
Trezy <t@trezy.com>
2021-12-19 14:24:10 -0600

    feat(exports): add `add`, `subtract`, `multiply`, and `divide` methods directly to main exports

--- test/index.test.js
+++ test/index.test.js
@@ -12,13 +12,45 @@ import * as AllExports from '../lib/index.js'
 
 
 
+const EXPECTED_EXPORTS = [
+  'add',
+  'BigMath',
+  'BigFloat',
+  'divide',
+  'isNumberLike',
+  'multiply',
+  'subtract',
+]
+
 describe('exports', function () {
+  // EXPECTED_EXPORTS.forEach(exportName => {
+  //   it(`exports \`${exportName}\``, function () {
+  //     expect(AllExports).to.have.property(exportName)
+  //   })
+  // })
+
   it('exports `BigFloat`', function () {
     expect(AllExports).to.have.property('BigFloat')
   })
 
   it('exports `BigMath`', function () {
     expect(AllExports).to.have.property('BigMath')
+  })
+
+  it('exports `add`', function () {
+    expect(AllExports).to.have.property('add')
+  })
+
+  it('exports `subtract`', function () {
+    expect(AllExports).to.have.property('subtract')
+  })
+
+  it('exports `multiply`', function () {
+    expect(AllExports).to.have.property('add')
+  })
+
+  it('exports `divide`', function () {
+    expect(AllExports).to.have.property('divide')
   })
 
   it('exports `isNumberLike`', function () {
